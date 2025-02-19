aadafeabf73bcb1a34085a4de89674b584f23ae9
chuckyawwal <yorubamusty@gmail.com>
2025-02-19 23:41:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'CHUCKY',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "MUHAMMAD CHUCKY",
+  botOwner: 'Chucky', // Your name
+  ownerNumber: '2349014931184', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
